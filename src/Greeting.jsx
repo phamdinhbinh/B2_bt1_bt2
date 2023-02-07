2984736ab1d2ae4277665ce5ad407611c5f0af4d
phamdinhbinh <phamdinhbinhonly1@gmail.com>
2023-02-07 14:11:53 +0700

    bt1 bt2

---
+++ src/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Greeting({myName1}) {
+   
+    return (
+    <div>
+         Greeting : {myName1}
+    </div>
+    
+    );
+   }
---
+++ src/UserForm.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+import Greeting from './Greeting';
+export default function UserForm() {
+    const [userInput, setUserInput] = useState("");
+    const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+    };
+    // let myname= ' alo '
+    return (
+    <div>
+        <span>Name : </span>
+    <input type="text"
+     id='name' 
+     placeHolder='Nhập tên'
+     onChange={handleInputChange}
+     />
+    
+     <Greeting myName1={userInput}/>
+    </div>
+    );
+   }
